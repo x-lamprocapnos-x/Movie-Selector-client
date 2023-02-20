e88324b886bd4ed328eaec45340720965fa6a516
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-02-20 17:26:44 -0500

    update index.jsx
    - update ' < "

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import { createRoot} from 'react-dom/client';
+import { createRoot} from "react-dom/client";
 //import statement to indicate that you need to buncle `./index.scss
-import './index.scss';
+import "./index.scss";
 //main component (will eventually use all others)
 const MyFlixApplication = () => {
     return(
-        <div className='my-flix'>
+        <div className="my-flix">
             <div>Good Morning</div>
         </div>
     );
 };
 //finds the root of your app
-const container = document.querySelector('#root');
+const container = document.querySelector("#root");
 const root = createRoot(container);
 //tells react to render your app in the root DOM element
 root.render(<MyFlixApplication/>);
