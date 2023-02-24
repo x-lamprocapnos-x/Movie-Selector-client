92b6d6ab8e97348aec741d99be91c74688110918
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-02-24 18:42:49 -0500

    create main-view.jsx
    - create main-view.jsx file
    - export Mainview
    - add content

---
+++ src/components/mainView/main-view.jsx
@@ -0,0 +1,9 @@
+export const MainView = () => {
+    return(
+        <div>
+            <div>"Title: Cloudy With a Chance of Meatballs"</div>
+            <div>"Title: Up"</div>
+            <div>"Title: Gnomeo and Juliet"</div>
+        </div>
+    );
+};
