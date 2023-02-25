6cae0814766d45bee4a1cca29bbc78d903699cfe
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-02-25 12:02:47 -0500

    update main-view.jsx
    - import useState
    - create movie array function

--- src/components/mainView/main-view.jsx
+++ src/components/mainView/main-view.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import { MovieCard} from "../movieCard/movie-card";
+
 export const MainView = () => {
-    return(
-        <div>
-            <div>"Title: Cloudy With a Chance of Meatballs"</div>
-            <div>"Title: Up"</div>
-            <div>"Title: Gnomeo and Juliet"</div>
-        </div>
-    );
+    const [movies, setMovies] = useState([
+        { id: 1, Title: "Up" },
+        { id: 2, Title: "Gnomeo and Juliet" },
+        { id: 3, Title: "Cloudy with a Chance of Meatballs" }
+    ]);
+
+    if (movies.length === 0) {
+        return <div>Please add Movies!</div>
+    } else {
+        return (
+            <div>
+                {movies.map((movie) => {
+                    return <MovieCard movie={movie}/>
+                })}
+            </div>
+        );
+    }
 };
