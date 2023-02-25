d39acee1ef806b9fc37ed7bd8dac3f27e6690606
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-02-25 18:32:45 -0500

    update index.jsx
    - add semicolon after MainView import statement
    - add semicolol after MainView object in app function

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
-import MainView from "./components/mainView/main-view"
+import MainView from "./components/mainView/main-view";
 //import statement to indicate that you need to bundle `./index.scss
 import "./index.scss";
 //main component (will eventually use all others)
 const MyFlixApplication = () => {
     return(
         <div className="my-flix">
-            <MainView/>
+            <MainView/>;
         </div>
     );
 };
