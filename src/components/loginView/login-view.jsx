07a4ec32f53f81ba0b3dd8fcd8538251d8ae405e
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-03-14 15:54:33 -0400

    update login-view.jsx/scss
    login-view.jsx
    - import login-view.scss
    - import Button from react-bootstrap
    - import Form from react-bootstrap
    - replace form < Form
    - replace button < Button
    - add classname "submit-button
    
    login-view.scss
    - add submit-button class styling

--- src/components/loginView/login-view.jsx
+++ src/components/loginView/login-view.jsx
@@ -1,4 +1,7 @@
+import "./login-view.scss"
 import { React, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 
 
@@ -13,8 +16,8 @@ export const LoginView = ({ onLoggedIn }) => {
             Password: password
         };
 
-// login link
-        fetch("https://movie-selector.onrender.com/login", { 
+        // login link
+        fetch("https://movie-selector.onrender.com/login", {
             method: "Post",
             headers: {
                 "Content-Type": "application/json"
@@ -37,25 +40,28 @@ export const LoginView = ({ onLoggedIn }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username: <input
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formUsername">
+            <Form.Label>Username:</Form.Label>
+                 <Form.Control
                     type="text"
                     value={username}
                     minLength="7"
                     onChange={(e) => setUsername(e.target.value)}
                     required
                 />
-            </label>
-            <label>
-                Password: <input
+            </Form.Group>
+            
+            <Form.Group controlId="formPassword">
+            <Form.Label>Password:</Form.Label>
+                 <Form.Control
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+            </Form.Group>
+            <Button variant="" type="submit" className="submit-button">Submit</Button>
+        </Form>
     );
 };
