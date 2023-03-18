a64eef06bbb933b0b9b70fcc2f30dc9166659e95
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-03-17 20:08:25 -0400

    update signup-view.jsx
    - import Button from react-bootstrap
    - import Form from react-bootstrap
    - update <form> with <Form>
    - add <Form.Group>
    - update <label> with <Form.Label>
    - update <input> with <Form/Control>
    - update <button> with <Button>

--- src/components/signUpView/signup-view.jsx
+++ src/components/signUpView/signup-view.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import  Button  from "react-bootstrap/Button";
+import  Form  from "react-bootstrap/Form";
 
 export const SignupView = () => {
     const [username, setUsername] = useState("");
@@ -15,15 +17,15 @@ export const SignupView = () => {
             Email: email,
             Birthday: birthday
         };
-     //create profile link
+        //create profile link
         fetch("https://movie-selector.onrender.com/users", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
         }).then((response) => {
-            if(response.ok){
+            if (response.ok) {
                 alert("Signup Successful");
                 window.location.reload();
             } else {
@@ -32,46 +34,53 @@ export const SignupView = () => {
         });
     };
 
-    return(
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username: 
-                <input 
-                 type="text"
-                 value={username}
-                 onChange={(e) => setUsername(e.target.value)}
-                 required
-                 minLength="7"
-                 />
-            </label>
-            <label>
-                Password:
-                <input
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    minLength="7"
+                />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+            <Form.Label>Password:</Form.Label>
+                
+                <Form.Control
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
-            </label>
-            <label>
-                Email:
-                <input
+            </Form.Group>
+
+            <Form.Group controlId="formEmail">
+            <Form.Label>Email</Form.Label>
+                
+                <Form.Control
                     type="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
                 />
-            </label>
-            <label>
+            
+            </Form.Group>
+
+            <Form.Group controlId="formBirthday"></Form.Group>
+            <Form.Label></Form.Label>
                 Birthday:
-                <input
-                type="date"
-                value={birthday}
-                onChange={(e) => setBirthday(e.target.value)}
-                required
+                <Form.Control
+                    type="date"
+                    value={birthday}
+                    onChange={(e) => setBirthday(e.target.value)}
+                    required
                 />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+            
+            <Button variant="primary" type="submit" className="submit-button">Submit</Button>
+        </Form>
     );
 };
