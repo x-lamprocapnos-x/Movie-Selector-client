c5f2002251f2b4fc86b5a0b30b8a73425d941540
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-03-20 19:09:38 -0400

    update movie-view.jsx
    - add Genre
    - add actors
    - add descriptions
    - format document

--- src/components/movieView/movie-view.jsx
+++ src/components/movieView/movie-view.jsx
@@ -1,6 +1,6 @@
 import "./movie-view.scss"
 
-export const MovieView = ({ movie , onBackClick}) => {
+export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
             <div>
@@ -9,12 +9,27 @@ export const MovieView = ({ movie , onBackClick}) => {
                 </div>
                 <div>
                     <span>Title: </span>
-                    <span>{movie.title}</span>
+                    <span> {movie.title} </span>
                 </div>
-                <span>Director: </span>
-                <span>{movie.director.map((director) => director.Name).join(", ")}</span>
+                <div>
+                    <span>Genre: </span>
+                    <span> {movie.genre.map((genre) => genre.Name).join(", ")} </span>
+                </div>
+                <div>
+                    <span>Director: </span>
+                    <span> {movie.director.map((director) => director.Name).join(", ")} </span>
+                </div>
+                <div>
+                    <span>Actors: </span>
+                    <span> {movie.actors} </span>
+                </div>
+                <div>
+                    <span>Description: </span>
+                    <span> {movie.description} </span>
+                </div>
+
             </div>
-            <button onClick={onBackClick} className="back-button" style={{ cursor:"pointer" }}>Back</button>
+            <button onClick={onBackClick} className="back-button" style={{ cursor: "pointer" }}>Back </button>
         </div>
     );
 };
