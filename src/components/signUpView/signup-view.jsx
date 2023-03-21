0d06cb4da1e7230ecf8836ab84674611819e9486
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-03-21 18:00:09 -0400

    update signup-view.jsx
    - move Button and Form into singular import statement

--- src/components/signUpView/signup-view.jsx
+++ src/components/signUpView/signup-view.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import  Button  from "react-bootstrap/Button";
-import  Form  from "react-bootstrap/Form";
+import { Button, Form } from "react-bootstrap";
 
 export const SignupView = () => {
     const [username, setUsername] = useState("");
