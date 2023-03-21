4153fb9075eeeff10a5743c19210048bec595797
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-03-21 16:14:33 -0400

    update movie-card.jsx
    - add className to MovieCard <Card>

--- src/components/movieCard/movie-card.jsx
+++ src/components/movieCard/movie-card.jsx
@@ -3,7 +3,7 @@ import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <Card>
+        <Card className="h-100">
             <Card.Img variant="top" src={movie.image} />
             <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
