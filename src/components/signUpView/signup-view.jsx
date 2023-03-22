e4a079c6507c53fb2549b58baedb00fe35583683
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-03-22 19:57:39 -0400

    update signup-view
    - import Card from react-bootstrap
    - import Row from react-bootstrap
    - add Card and Row
    - Format document

--- src/components/signUpView/signup-view.jsx
+++ src/components/signUpView/signup-view.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Card, Row } from "react-bootstrap";
 
 export const SignupView = () => {
     const [username, setUsername] = useState("");
@@ -34,52 +34,56 @@ export const SignupView = () => {
     };
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
-                <Form.Control
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                    minLength="7"
-                />
-            </Form.Group>
+        <Row>
+            <Card className="sigBox">
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="formUsername">
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                            minLength="7"
+                        />
+                    </Form.Group>
 
-            <Form.Group controlId="formPassword">
-            <Form.Label>Password:</Form.Label>
-                
-                <Form.Control
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </Form.Group>
+                    <Form.Group controlId="formPassword">
+                        <Form.Label>Password:</Form.Label>
 
-            <Form.Group controlId="formEmail">
-            <Form.Label>Email</Form.Label>
-                
-                <Form.Control
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            
-            </Form.Group>
+                        <Form.Control
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </Form.Group>
 
-            <Form.Group controlId="formBirthday"></Form.Group>
-            <Form.Label></Form.Label>
-                Birthday:
-                <Form.Control
-                    type="date"
-                    value={birthday}
-                    onChange={(e) => setBirthday(e.target.value)}
-                    required
-                />
-            
-            <Button variant="primary" type="submit" className="submit-button">Submit</Button>
-        </Form>
+                    <Form.Group controlId="formEmail">
+                        <Form.Label>Email</Form.Label>
+
+                        <Form.Control
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+
+                    </Form.Group>
+
+                    <Form.Group controlId="formBirthday"></Form.Group>
+                    <Form.Label></Form.Label>
+                    Birthday:
+                    <Form.Control
+                        type="date"
+                        value={birthday}
+                        onChange={(e) => setBirthday(e.target.value)}
+                        required
+                    />
+
+                    <Button variant="primary" type="submit" className="submit-button">Submit</Button>
+                </Form>
+            </Card>
+        </Row>
     );
 };
