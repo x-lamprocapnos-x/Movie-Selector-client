656df8b1f10d94d1d3c048cb59a6aa55b99c97fb
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-03-29 17:36:19 -0400

    update navigation-bar.jsx
    - add links for unauthorized and authorized users
    - update Navbar.Brand

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,17 +1,31 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export const NavigationBar = ({ user, onLoggedOut}) => {
-    return(
+export const NavigationBar = ({ user, onLoggedOut }) => {
+    return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand as={Link} to="/">
-                    Movies App
+                <Navbar.Brand
+                    as={Link} to="/">
+                    Movie Selector
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        {/* add links here */}
+                        {!user && (
+                            <>
+                                <Nav.Link as={Link} to="/login" />
+                                <Nav.Link as={Link} to="/signup" />
+                            </>
+                        )}
+                        {user && (
+                            <>
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+                            <Nav.Link as={Link} onClick={onLoggedOut}>Logout</Nav.Link>
+                            </>
+                        )}
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
