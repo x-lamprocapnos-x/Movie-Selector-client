f69fac4c80435259a8c91db601140000c0653fa6
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-05-16 18:31:35 -0400

    Create reducers
    - create reducers folder
    - create reducers.js
    - add reducers for corresponding actions

---
+++ src/reducers/reducers.js
@@ -0,0 +1,31 @@
+import {combineReducers} from "redux";
+import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
+
+function movies(state=[], action){
+    switch(action.type){
+        case SET_MOVIES: 
+        return action.value;
+        default: return state;
+    }
+}
+
+function filter(state=[], action){
+    switch(action.type){
+        case SET_FILTER:
+            return action.value;
+            default: return state;
+    }
+}
+
+function user(state=[], action){
+    switch(action.type){
+        case SET_USER:
+            return action.value;
+            default: return state;
+    }
+}
+
+
+
+const movieApp = combineReducers({movies});
+export default movieApp;
