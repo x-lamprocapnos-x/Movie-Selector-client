8f0bb9a9cda7c1b1462d36823d3d9c4475fe576b
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-05-16 20:41:15 -0400

    Update index.jsx
    - remove "{}" from MainView
    - import configureStore from reduxjs/toolkit
    - import Provider from react-redux
    - import movieApp from reducers
    - create const variable store
    - implement provider and store

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,26 @@
 import { createRoot } from "react-dom/client";
-import { MainView } from "./components/mainView/main-view";
+import  MainView  from "./components/mainView/main-view";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
 //import statement to indicate that you need to bundle `./index.scss
 import "./index.scss";
 //main component (will eventually use all others)
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from "react-redux";
+import movieApp from "./reducers/reducers";
+const store = configureStore({ reducer: movieApp });
 const MyFlixApplication = () => {
-    return(
-        <Container>
-            <MainView/>
-        </Container>
+    return (
+        <Provider store={store}>
+            <Container>
+                <MainView />
+            </Container>
+        </Provider>
     );
 };
 //finds the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 //tells react to render your app in the root DOM element
-root.render(<MyFlixApplication/>);
+root.render(<MyFlixApplication />);
 
