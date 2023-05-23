0f474e6a5ba9d8e84689f508ce8d05d436d64acc
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-05-22 20:27:47 -0400

    update reducers.js
    - SET_FILTER "state="" > ...=[]"
    - SET_FILTER "state={} > ...=[]"
    - add filter and user to combineReducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -9,7 +9,7 @@ function movies(state=[], action){
     }
 }
 
-function filter(state=[], action){
+function filter(state="", action){
     switch(action.type){
         case SET_FILTER:
             return action.value;
@@ -17,7 +17,7 @@ function filter(state=[], action){
     }
 }
 
-function user(state=[], action){
+function user(state={}, action){
     switch(action.type){
         case SET_USER:
             return action.value;
@@ -27,5 +27,5 @@ function user(state=[], action){
 
 
 
-const movieApp = combineReducers({movies});
+const movieApp = combineReducers({movies, filter, user});
 export default movieApp;
